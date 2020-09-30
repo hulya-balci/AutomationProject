1d7592b13c4953d24a832cf2a0978b0bf472e5f8
hulya balci <hulyabalci07@gmail.com>
2020-09-30 22:42:45 +0200

    more changes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello class eu3");
-        //making changes and gonna commit it
+        //making changes and gonna commit it//i do more changes to track git
+        //more probably i ll work
     }
 }
